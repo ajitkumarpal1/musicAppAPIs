995734f3e31d0085f5e116a0698c6898f54b1453
ajitkumarpal1 <66736656+ajitkumarpal1@users.noreply.github.com>
2024-08-18 02:19:57 +0530

    auth remove in sing by id

--- routes/song.js
+++ routes/song.js
@@ -6,8 +6,8 @@ import { songService } from "../src/controllers/song_controller.js";
 export const songRouter = express.Router();
 
 songRouter.get("/list",songService.list)
-songRouter.use(auth)
 songRouter.get("/song-by-id/:songId",songService.getSongById)
+songRouter.use(auth)
 songRouter.post("/push-song-in-playlist",songService.pushSongInPlaylist)
 songRouter.post("/create-play-list",songService.createPlayList)
 songRouter.get("/get-play-list",songService.getPlayList)
